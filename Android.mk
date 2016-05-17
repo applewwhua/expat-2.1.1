8402057126188530a773b54779853ef2c8c5263b
Andy Wang <applewwhua@hotmail.com>
2016-05-17 14:13:42 +0800

    Build libexpat.so for Android

--- Android.mk
+++ Android.mk
@@ -18,64 +18,64 @@ common_CFLAGS := \
 common_C_INCLUDES += \
 	$(LOCAL_PATH)/lib
 
-# For the host
-# =====================================================
-
-# Host static library
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_CFLAGS_darwin += -fno-common
-
-LOCAL_MODULE:= libexpat
-LOCAL_MODULE_HOST_OS := darwin linux windows
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
-
-LOCAL_MULTILIB := both
-
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-# Host shared library
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_CFLAGS_darwin += -fno-common
-
-LOCAL_MODULE:= libexpat-host
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
-LOCAL_MULTILIB := both
-
-include $(BUILD_HOST_SHARED_LIBRARY)
-
-
-# For the device
-# =====================================================
-
-# Device static library
-include $(CLEAR_VARS)
-
-ifeq ($(TARGET_ARCH),arm)
-    LOCAL_SDK_VERSION := 8
-else
-    LOCAL_SDK_VERSION := 9
-endif
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_MODULE:= libexpat_static
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
-LOCAL_MODULE_TAGS := optional
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
-
-include $(BUILD_STATIC_LIBRARY)
+# # For the host
+# # =====================================================
+# 
+# # Host static library
+# include $(CLEAR_VARS)
+# 
+# LOCAL_SRC_FILES := $(common_SRC_FILES)
+# LOCAL_CFLAGS += $(common_CFLAGS)
+# LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+# 
+# LOCAL_CFLAGS_darwin += -fno-common
+# 
+# LOCAL_MODULE:= libexpat
+# LOCAL_MODULE_HOST_OS := darwin linux windows
+# LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
+# 
+# LOCAL_MULTILIB := both
+# 
+# include $(BUILD_HOST_STATIC_LIBRARY)
+# 
+# # Host shared library
+# include $(CLEAR_VARS)
+# 
+# LOCAL_SRC_FILES := $(common_SRC_FILES)
+# LOCAL_CFLAGS += $(common_CFLAGS)
+# LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+# 
+# LOCAL_CFLAGS_darwin += -fno-common
+# 
+# LOCAL_MODULE:= libexpat-host
+# LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
+# LOCAL_MULTILIB := both
+# 
+# include $(BUILD_HOST_SHARED_LIBRARY)
+# 
+# 
+# # For the device
+# # =====================================================
+# 
+# # Device static library
+# include $(CLEAR_VARS)
+# 
+# ifeq ($(TARGET_ARCH),arm)
+#     LOCAL_SDK_VERSION := 8
+# else
+#     LOCAL_SDK_VERSION := 9
+# endif
+# 
+# LOCAL_SRC_FILES := $(common_SRC_FILES)
+# LOCAL_CFLAGS += $(common_CFLAGS)
+# LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+# 
+# LOCAL_MODULE:= libexpat_static
+# LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
+# 
+# include $(BUILD_STATIC_LIBRARY)
 
 # Device shared library
 include $(CLEAR_VARS)
---
+++ build-libexpat-android.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/opt/android-ndk-r11c/ndk-build NDK_PROJECT_PATH=./ APP_BUILD_SCRIPT=./Android.mk
